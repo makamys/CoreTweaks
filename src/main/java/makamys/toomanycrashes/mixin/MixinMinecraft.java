6d8e9783991c3aced23e1e6253c78d2b426df90c
makamys <makamys@outlook.com>
2021-05-18 00:45:24 +0200

    Clean up MixinMinecraft, don't log mixin injection

--- src/main/java/makamys/toomanycrashes/mixin/MixinMinecraft.java
+++ src/main/java/makamys/toomanycrashes/mixin/MixinMinecraft.java
@@ -1,21 +1,13 @@
 package makamys.toomanycrashes.mixin;
 
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import makamys.toomanycrashes.GuiFatalErrorScreen;
 import makamys.toomanycrashes.TooManyCrashes;
-import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiMainMenu;
-import net.minecraft.client.gui.GuiMemoryErrorScreen;
-import net.minecraft.world.IBlockAccess;
-
 
 @Mixin(Minecraft.class)
 abstract class MixinMinecraft {
@@ -28,7 +20,6 @@ abstract class MixinMinecraft {
     
     private Throwable theError;
     
-    // void net.minecraft.client.Minecraft.runGameLoop()
     @Redirect(method = "Lnet/minecraft/client/Minecraft;run()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;runGameLoop()V"))
     public void run(Minecraft minecraft) {
         try {
@@ -42,11 +33,9 @@ abstract class MixinMinecraft {
             TooManyCrashes.handleCrash(null);
         }
     }
-    // void net.minecraft.client.Minecraft.displayGuiScreen(GuiScreen p_147108_1_)
-    // net.minecraft.client.gui.GuiScreen
     
     @Redirect(method = "Lnet/minecraft/client/Minecraft;run()V", at = @At(value = "INVOKE",
-            target = "Lnet/minecraft/client/Minecraft;displayGuiScreen(Lnet/minecraft/client/gui/GuiScreen;)V", args = {"log=true"}))
+            target = "Lnet/minecraft/client/Minecraft;displayGuiScreen(Lnet/minecraft/client/gui/GuiScreen;)V"))
     public void redirectDisplayGuiScreen(Minecraft minecraft) {
         minecraft.displayGuiScreen(new GuiFatalErrorScreen(theError));
     }
