e9d5fccfc6d9b88f64d8a0f2b1548deb5befc11f
makamys <makamys@outlook.com>
2021-06-16 19:10:49 +0200

    Add some config descriptions

--- src/main/java/makamys/toomanycrashes/Config.java
+++ src/main/java/makamys/toomanycrashes/Config.java
@@ -21,10 +21,10 @@ public class Config {
         Configuration config = new Configuration(new File(Launch.minecraftHome, "config/toomanycrashes.cfg"));
         
         config.load();
-        crashHandler = config.get("Tweaks", "crashHandler", true, "Handle crashes without exiting the game. May cause graphical glitches in the newly loaded game!").getBoolean();
+        crashHandler = config.get("Tweaks", "crashHandler", true, "Lets you survive crashes without the game exiting, usually. May cause graphical glitches in the newly loaded game!").getBoolean();
         restoreTravelSound = config.get("Tweaks", "restoreTravelSound", true, "Restore interdimensional travel sound (travel.ogg). Fixes MC-233, fixed in 1.9").getBoolean();
-        getPendingBlockUpdates = config.get("Optimizations", "getPendingBlockUpdates", true).getBoolean();
-        clientChunkMap = config.get("Optimizations", "clientChunkMap", true).getBoolean();
+        getPendingBlockUpdates = config.get("Optimizations", "getPendingBlockUpdates", true, "Optimizes WorldServer#getPendingBlockUpdates. OptiFine also does this, but this won't have an effect when OF is present, so there's no conflict.").getBoolean();
+        clientChunkMap = config.get("Optimizations", "clientChunkMap", true, "Faster implementation of ChunkProviderClient#chunkMapping. From 1.16 (I don't know when exactly it was added). Might be buggy when travelling between dimensions?").getBoolean();
         TMCCommand = config.get("Misc", "TMCCommand", true).getBoolean();
         spikeThreshold = config.get("Misc", "spikeThreshold", 30).getInt();
         printActive = config.get("Misc", "printActive", true).getBoolean();
