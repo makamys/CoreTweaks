d7a17fa9f62ddb8cab6851785dce8f600953ca53
makamys <makamys@outlook.com>
2021-06-19 19:15:00 +0200

    Clean up jad sociverer

--- src/main/java/makamys/toomanycrashes/ASMModParserTransformer.java
+++ src/main/java/makamys/toomanycrashes/ASMModParserTransformer.java
@@ -3,9 +3,7 @@ package makamys.toomanycrashes;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.InsnNode;
 import org.objectweb.asm.tree.MethodNode;
 
 import codechicken.lib.asm.ASMReader;
@@ -31,14 +29,9 @@ public class ASMModParserTransformer implements IClassTransformer {
 				ClassNode classNode = new ClassNode();
 				ClassReader classReader = new ClassReader(bytes);
 				classReader.accept(classNode, 0);
-				//Lists.newArr
 				MethodNode emptyConstructor = new MethodNode(Opcodes.ACC_PUBLIC, "<init>", "()V", null, null);
-				//MethodNode emptyConstructor = new MethodNode(Opcodes.ACC_PUBLIC, "<init>", "()V", null, null);
 				
-				
-				//Lists.newArrayList(emptyConstructor.instructions.iterator())
 				emptyConstructor.instructions.add(ASMReader.loadResource("/assets/toomanycrashes/asm/tweaks.asm").get("ASMModParserEmptyConstructor").rawListCopy());
-				//emptyConstructor.instructions.add(new InsnNode(Opcodes.ARETURN));
 				classNode.methods.add(emptyConstructor);
 				
 				writer = new ClassWriter(ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
