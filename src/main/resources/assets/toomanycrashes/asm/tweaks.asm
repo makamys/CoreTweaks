a7cd21772c119703c1814bc59228072f4d7d7a0c
makamys <makamys@outlook.com>
2021-06-19 19:43:52 +0200

    Get rid of that CodeChickenCore dependency

--- src/main/resources/assets/toomanycrashes/asm/tweaks.asm
+++
@@ -1,7 +0,0 @@
-list ASMModParserEmptyConstructor
-ALOAD 0
-INVOKESPECIAL java/lang/Object.<init>()V
-ALOAD 0
-INVOKESTATIC com/google/common/collect/Lists.newLinkedList()Ljava/util/LinkedList;
-PUTFIELD cpw/mods/fml/common/discovery/asm/ASMModParser.annotations:Ljava/util/LinkedList;
-RETURN
