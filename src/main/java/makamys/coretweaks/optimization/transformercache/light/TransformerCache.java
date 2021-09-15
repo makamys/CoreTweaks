353f289df6929dd4993768d1091db7eb60cbb951
makamys <makamys@outlook.com>
2021-09-15 05:01:52 +0200

    Set up basic data structure and serialization

--- src/main/java/makamys/coretweaks/optimization/transformercache/light/TransformerCache.java
+++ src/main/java/makamys/coretweaks/optimization/transformercache/light/TransformerCache.java
@@ -1,11 +1,23 @@
 package makamys.coretweaks.optimization.transformercache.light;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import com.esotericsoftware.kryo.unsafe.UnsafeInput;
+import com.esotericsoftware.kryo.unsafe.UnsafeOutput;
 
 import cpw.mods.fml.common.asm.transformers.DeobfuscationTransformer;
 import cpw.mods.fml.relauncher.ReflectionHelper;
+import makamys.coretweaks.CoreTweaks;
 import net.minecraft.launchwrapper.IClassTransformer;
 import net.minecraft.launchwrapper.Launch;
 import net.minecraft.launchwrapper.LaunchClassLoader;
@@ -14,12 +26,34 @@ public class TransformerCache {
     
     public static TransformerCache instance;
     
+    private Map<String, TransformerData> data = new HashMap<>();
+    
+    private final File file = CoreTweaks.getDataFile("transformerCache.dat");
+    private final Kryo kryo = new Kryo();
+    
     public static void init() {
         instance = new TransformerCache();
     }
     
     public TransformerCache() {
         hookClassLoader();
+        loadData();
+        
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    if(!file.exists()) {
+                        file.getParentFile().mkdirs();
+                        file.createNewFile();
+                    }
+                    try(Output output = new UnsafeOutput(new BufferedOutputStream(new FileOutputStream(file, true)))) {
+                        kryo.writeObject(output, data);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }}, "CoreTweaks transformer cache save thread"));
     }
 
     private void hookClassLoader() {
@@ -29,6 +63,18 @@ public class TransformerCache {
             if(transformers.get(i) instanceof DeobfuscationTransformer) {
                 System.out.println("Found deobfuscation transformer, hooking it with cached one");
                 transformers.set(i, new CachedNameTransformer((DeobfuscationTransformer)transformers.get(i)));
+            }
+        }
+    }
+    
+    private void loadData() {
+        kryo.setRegistrationRequired(false);
+        
+        if(file.exists()) {
+            try(Input is = new UnsafeInput(new BufferedInputStream(new FileInputStream(file)))) {
+                data = kryo.readObject(is, Map.class);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
@@ -45,6 +91,10 @@ public class TransformerCache {
 
     public void putCached(IClassTransformer transformer, String name, String transformedName, byte[] result) {
         // TODO Auto-generated method stub
+        
+    }
+    
+    public class TransformerData {
         
     }
 }
