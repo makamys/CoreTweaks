0571ae43da2ca16b08a292685ec6c17ac4ab94cf
makamys <makamys@outlook.com>
2022-05-16 13:41:36 +0200

    Fix LightFixStare crashing server

--- src/main/java/makamys/coretweaks/tweak/LightFixStare.java
+++ src/main/java/makamys/coretweaks/tweak/LightFixStare.java
@@ -1,26 +1,33 @@
 package makamys.coretweaks.tweak;
 
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 
 public class LightFixStare {
     
     public static void postPlayerCheckLight(World world) {
         if (!world.playerEntities.isEmpty()) {
-            int var1 = world.rand.nextInt(world.playerEntities.size());
-            EntityPlayer var2 = (EntityPlayer)world.playerEntities.get(var1);
-            MovingObjectPosition hit = var2.rayTrace(1000, 0);
-            if(hit != null) {
-                int posX = hit.blockX;
-                int posY = hit.blockY;
-                int posZ = hit.blockZ;
-                int var3 = posX + world.rand.nextInt(11) - 5;
-                int var4 = posY + world.rand.nextInt(11) - 5;
-                int var5 = posZ + world.rand.nextInt(11) - 5;
-                world.func_147451_t(var3, var4, var5);
-            }
+            int playerIndex = world.rand.nextInt(world.playerEntities.size());
+            EntityPlayer player = (EntityPlayer)world.playerEntities.get(playerIndex);
+            MovingObjectPosition hit = rayTrace(player, 1000);
+            int posX = hit.blockX;
+            int posY = hit.blockY;
+            int posZ = hit.blockZ;
+            int var3 = posX + world.rand.nextInt(11) - 5;
+            int var4 = posY + world.rand.nextInt(11) - 5;
+            int var5 = posZ + world.rand.nextInt(11) - 5;
+            world.func_147451_t(var3, var4, var5);
         }
+    }
+    
+    private static MovingObjectPosition rayTrace(EntityLivingBase entity, double reach) {
+        Vec3 vec3 = Vec3.createVectorHelper(entity.posX, entity.posY, entity.posZ);
+        Vec3 vec31 = entity.getLook(1);
+        Vec3 vec32 = vec3.addVector(vec31.xCoord * reach, vec31.yCoord * reach, vec31.zCoord * reach);
+        return entity.worldObj.func_147447_a(vec3, vec32, true, false, true);
     }
     
 }
