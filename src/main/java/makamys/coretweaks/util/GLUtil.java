78f165666d8952b5af96659bc263951dc55857a6
makamys <makamys@outlook.com>
2022-06-08 12:50:27 +0200

    Unbind GL buffer objects after crash

--- src/main/java/makamys/coretweaks/util/GLUtil.java
+++ src/main/java/makamys/coretweaks/util/GLUtil.java
@@ -5,6 +5,8 @@
 package makamys.coretweaks.util;
 
 import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL30.*;
 
 import static makamys.coretweaks.CoreTweaks.LOGGER;
 
@@ -22,6 +24,11 @@ public class GLUtil {
         if(glGetInteger(GL_LIST_INDEX) != 0) {
             glEndList();
         }
+        
+        glBindVertexArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+        
         // TODO restore other stuff too, like attrib stack
         
         LOGGER.debug("GL state after restore:");
