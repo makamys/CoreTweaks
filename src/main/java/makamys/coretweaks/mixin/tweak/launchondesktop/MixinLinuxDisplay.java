d68a6761de0b98682d9f9eeeb4d1b3ad4adbeb1f
makamys <makamys@outlook.com>
2022-07-26 15:45:55 +0200

    Wait for xprop to exit
    Hopefully fixes window rarely failing to get redirected

--- src/main/java/makamys/coretweaks/mixin/tweak/launchondesktop/MixinLinuxDisplay.java
+++ src/main/java/makamys/coretweaks/mixin/tweak/launchondesktop/MixinLinuxDisplay.java
@@ -2,6 +2,7 @@ package makamys.coretweaks.mixin.tweak.launchondesktop;
 
 import static makamys.coretweaks.CoreTweaks.LOGGER;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.spongepowered.asm.mixin.Mixin;
@@ -25,10 +26,16 @@ public class MixinLinuxDisplay {
 	public void afterCreateWindow(CallbackInfo ci) {
 		try {
 			LOGGER.info("Attempting to set _NET_WM_DESKTOP property of window (" + current_window + ") to " + JVMArgs.LAUNCH_ON_DESKTOP);
-			Runtime.getRuntime().exec("xprop -id " + current_window + " -f _NET_WM_DESKTOP 32c -set _NET_WM_DESKTOP 0x" + JVMArgs.LAUNCH_ON_DESKTOP);
+			Process proc = new ProcessBuilder("xprop", "-id", String.valueOf(current_window), "-f", "_NET_WM_DESKTOP", "32c", "-set", "_NET_WM_DESKTOP", "0x" + JVMArgs.LAUNCH_ON_DESKTOP)
+			.redirectOutput(new File("/dev/null")).redirectError(new File("/dev/null")).start();
+			proc.waitFor();
 			
-			LOGGER.info("Success!");
-		} catch (IOException/* | InterruptedException */e) {
+			if(proc.exitValue() == 0) {
+				LOGGER.info("Success!");	
+			} else {
+				throw new IllegalStateException("Exit code: " + proc.exitValue());
+			}
+		} catch (IOException | InterruptedException | IllegalStateException e) {
 			LOGGER.error("Failed to set _NET_WM_DESKTOP property of window. Is xprop installed?");
 			e.printStackTrace();
 		}
