31e3af99a9d3d54ecb020b774687871b63b7571f
makamys <makamys@outlook.com>
2022-08-20 17:15:50 +0200

    Add null check to rayTrace in LightFixStare, again
    Fixes #4

--- src/main/java/makamys/coretweaks/tweak/LightFixStare.java
+++ src/main/java/makamys/coretweaks/tweak/LightFixStare.java
@@ -14,13 +14,15 @@ public class LightFixStare {
             int playerIndex = world.rand.nextInt(world.playerEntities.size());
             EntityPlayer player = (EntityPlayer)world.playerEntities.get(playerIndex);
             MovingObjectPosition hit = MCUtil.rayTrace(player, 1000);
-            int posX = hit.blockX;
-            int posY = hit.blockY;
-            int posZ = hit.blockZ;
-            int var3 = posX + world.rand.nextInt(11) - 5;
-            int var4 = posY + world.rand.nextInt(11) - 5;
-            int var5 = posZ + world.rand.nextInt(11) - 5;
-            world.func_147451_t(var3, var4, var5);
+            if(hit != null) {
+                int posX = hit.blockX;
+                int posY = hit.blockY;
+                int posZ = hit.blockZ;
+                int var3 = posX + world.rand.nextInt(11) - 5;
+                int var4 = posY + world.rand.nextInt(11) - 5;
+                int var5 = posZ + world.rand.nextInt(11) - 5;
+                world.func_147451_t(var3, var4, var5);
+            }
         }
     }
     
