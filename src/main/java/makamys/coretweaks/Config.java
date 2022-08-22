7d92377a031ebfdf024e3c93259641a80ddc20a5
makamys <makamys@outlook.com>
2022-08-22 10:02:50 +0200

    Document the ability to cancel world auto-load

--- src/main/java/makamys/coretweaks/Config.java
+++ src/main/java/makamys/coretweaks/Config.java
@@ -85,7 +85,7 @@ public class Config {
         config.load();
         
         config.addCustomCategoryComment("Tweaks", "In addition to these settings, there are some tweaks that are activated via JVM flags:\n" +
-        "    -Dcoretweaks.launchWorld=WORLD : Automatically loads the world with the folder name WORLD once the main menu is reached. WORLD can be left blank, in this case the most recently played world will be loaded.\n" +
+        "    -Dcoretweaks.launchWorld=WORLD : Automatically loads the world with the folder name WORLD once the main menu is reached. WORLD can be left blank, in this case the most recently played world will be loaded. Hold down shift when the main menu appears to cancel the automatic loading.\n" +
         "    -Dcoretweaks.launchMinimized : Launch Minecraft minimized. Only implemented on Windows at the moment.\n" +
         "    -Dcoretweaks.launchOnDesktop=NUMBER : Launch Minecraft on the virtual desktop with ordinal NUMBER. Only implemented on Linux at the moment. xprop has to be installed for it to work. Only tested on Openbox.");
         
