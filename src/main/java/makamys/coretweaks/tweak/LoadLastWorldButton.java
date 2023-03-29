a903a95df8f2617e43ab5b8d0fb91d8ea9eb18ee
makamys <makamys@outlook.com>
2023-03-29 17:09:51 +0200

    Fix continue button crashing with CustomMainMenu

--- src/main/java/makamys/coretweaks/tweak/LoadLastWorldButton.java
+++ src/main/java/makamys/coretweaks/tweak/LoadLastWorldButton.java
@@ -28,10 +28,28 @@ public class LoadLastWorldButton implements IModEventListener {
     @SubscribeEvent
     public void onGui(InitGuiEvent.Post event) {
         if(event.gui instanceof GuiMainMenu) {
-            GuiButton newWorld = ((List<GuiButton>)event.buttonList).stream().filter(b -> b.id == 1).findFirst().get();
-            int originalWidth = newWorld.width;
-            newWorld.width = originalWidth / 3 * 2 - 2;
-            loadLastWorldButton = new GuiButton(UPDATES_BUTTON_ID, newWorld.xPosition + originalWidth / 3 * 2 + 2, newWorld.yPosition, originalWidth - (originalWidth / 3 * 2 + 2), newWorld.height, I18n.format("gui.coretweaks.menu.continue", new Object[0]));
+            int buttonX, buttonY, buttonWidth, buttonHeight;
+            GuiButton newWorld = ((List<GuiButton>)event.buttonList).stream().filter(b -> b.id == 1).findFirst().orElse(null);
+            if(newWorld != null) {
+                // We found the single player button, squash it and put the continue button in the leftover space.
+                int originalWidth = newWorld.width;
+                newWorld.width = originalWidth / 3 * 2 - 2;
+                buttonX = newWorld.xPosition + originalWidth / 3 * 2 + 2;
+                buttonY = newWorld.yPosition;
+                buttonWidth = originalWidth - (originalWidth / 3 * 2 + 2);
+                buttonHeight = newWorld.height;
+            } else {
+                // Something is interfering (probably CustomMainMenu.) Let's just put it to the right of the single player button's original position.
+                int originalX = event.gui.width / 2 - 100;
+                int originalY = event.gui.height / 4 + 48;
+                int originalWidth = 200;
+                int originalHeight = 20;
+                buttonX = originalX + originalWidth + 4;
+                buttonY = originalY;
+                buttonWidth = originalWidth - (originalWidth / 3 * 2 + 2);
+                buttonHeight = originalHeight;
+            }
+            loadLastWorldButton = new GuiButton(UPDATES_BUTTON_ID, buttonX, buttonY, buttonWidth, buttonHeight, I18n.format("gui.coretweaks.menu.continue", new Object[0]));
             event.buttonList.add(loadLastWorldButton);
         }
     }
